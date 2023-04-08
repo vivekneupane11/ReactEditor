4407cca869eb5156be248dff2c0ca57a4587563b
vivekneupane11 <vivekneupane11@gmail.com>
2023-04-08 18:33:05 +0545

    mobile info

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div style={{display:'flex',flexDirection:'column',justifyContent:'center'}}>
       <header>
 	<div class="overlay">
-<h1>React Editor</h1>
-<h3 style={{color:'black'}}>SELECT ANY TEXT BELOW AND SEE THE MAGIC </h3>
-<p>For now only works in larger devices. Try it out in your laptop</p>
+<h1 className='heading'>React Editor</h1>
+<h3 className='info'  style={{color:'black'}}>SELECT ANY TEXT BELOW AND SEE THE MAGIC </h3>
+<p className='mobile-info' style={{background:'red'}}>For now only works in larger devices. Try it out in your laptop</p>
 		</div>
 </header>
     <div className="App" style={{width:'100%',display:'flex',justifyContent:'center'}}>
