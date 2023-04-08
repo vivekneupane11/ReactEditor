2a599b9e9444f16e43e33f11075f511104382672
vivekneupane11 <vivekneupane11@gmail.com>
2023-04-08 18:21:03 +0545

    init

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 	<div class="overlay">
 <h1>React Editor</h1>
 <h3 style={{color:'black'}}>SELECT ANY TEXT BELOW AND SEE THE MAGIC </h3>
+<p>For now only works in larger devices. Try it out in your laptop</p>
 		</div>
 </header>
     <div className="App" style={{width:'100%',display:'flex',justifyContent:'center'}}>
